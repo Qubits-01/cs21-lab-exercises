d0b928329c8550fe04d786be672c62924c06e73f
Marc Peejay Viernes <mpvviernes@gmail.com>
2022-10-04 19:05:09 +0800

    git fix

---
+++ lab_exer_4/floydwarshall.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int matrix[4][4] = {
+        0,
+        3,
+        -1,
+        5,
+        2,
+        0,
+        -1,
+        4,
+        -1,
+    };
+
+    return 0;
+}
