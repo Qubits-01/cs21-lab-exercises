282fdd3ce461cd46585657f2bbfb343a8862a23e
Marc Peejay Viernes <mpvviernes@gmail.com>
2022-10-24 21:25:25 +0800

    Working solution for the peg solitaire problem (w/o much test)

--- machine_prob_1/peg_solitaire/bin/peg_solitaire.dart
+++ machine_prob_1/peg_solitaire/bin/peg_solitaire.dart
@@ -1,7 +1,8 @@
 // Global variables.
-List<int> finalHole = [-1, -1];
-const int rows = 7;
-const int columns = 7;
+int finalHoleRow = -1;
+int finalHoleColumn = -1;
+const int noOfRows = 7;
+const int nowOfColumns = 7;
 const String emptyHole = '.';
 const String pegHole = 'o';
 const String invalidHole = 'x';
@@ -10,8 +11,12 @@ const String withPegFinalHole = 'O';
 
 // Dynamic variables.
 List<List<String>> boardState = [];
-List<List<int>> pegCoordinates = [];
-int noOfPegs = 0;
+// [noOfPegs, tempFinalHoleRow, tempFinalHoleColumn].
+List<int> metadata = [0, -1, -1];
+// The pegMovesSolution[0] holds the size of this said list.
+List<List<int>> pegMovesSolution = [
+  [0]
+];
 
 void main() {
   const List<String> input = [
@@ -25,30 +30,32 @@ void main() {
   ];
 
   // Fetch and decode input String.
-  for (int r = 0; r < rows; r++) {
+  for (int r = 0; r < noOfRows; r++) {
     List<String> tempRow = [];
-    for (int c = 0; c < columns; c++) {
+    for (int c = 0; c < nowOfColumns; c++) {
       tempRow.add(input[r][c]);
 
-      // Add the hole coordinate to the pegCoordinates list
-      // if it is a peg (i.e., 'o' or 'O').
-      if ((tempRow[c] == pegHole) || (tempRow[c] == withPegFinalHole)) {
-        pegCoordinates.add([r, c]);
-        noOfPegs++;
+      // Update the noOfPegs.
+      if (tempRow[c] == pegHole) metadata[0] = metadata[0] + 1;
 
+      // Determine the coordinate of the destination/final hole.
+      if (finalHoleRow < 0) {
         // Determine if it is a final hole with a peg.
         if (tempRow[c] == withPegFinalHole) {
           tempRow[c] = 'o';
-          finalHole = [r, c];
+          finalHoleRow = r;
+          finalHoleColumn = c;
+          metadata[0] = metadata[0] + 1;
+
+          continue;
         }
 
-        continue;
-      }
-
-      // Determine if it is a final hole without a peg.
-      if (tempRow[c] == withoutPegFinalHole) {
-        tempRow[c] = '.';
-        finalHole = [r, c];
+        // Determine if it is a final hole without a peg.
+        if (tempRow[c] == withoutPegFinalHole) {
+          tempRow[c] = '.';
+          finalHoleRow = r;
+          finalHoleColumn = c;
+        }
       }
     }
 
@@ -56,108 +63,221 @@ void main() {
   }
 
   print('boardState: $boardState');
-  print('finalHole: $finalHole');
-  print('pegCoordinates: $pegCoordinates');
-  print('noOfPegs: $noOfPegs');
+  print('finalHole: $finalHoleRow, $finalHoleColumn');
+  print('metadata: $metadata');
   print('\n');
 
-  noOfPegs = deleteElem(
-    list: pegCoordinates,
-    index: 2,
-    noOfPegs: noOfPegs,
-  );
-
-  print('boardState: $boardState');
-  print('finalHole: $finalHole');
-  print('pegCoordinates: $pegCoordinates');
-  print('noOfPegs: $noOfPegs');
-  print('\n');
-
-  solve(
+  bool isSolvable = solveSolitairePeg(
     boardState: boardState,
-    pegCoordinates: pegCoordinates,
-    noOfPegs: noOfPegs,
+    metadata: metadata,
   );
+
+  print('pegMovesSolution: $pegMovesSolution');
+  print('\n');
+
+  List<String> output = [];
+
+  // Print 'YES' if it is solvable, otherwise print 'NO'
+  String outputMsg = isSolvable ? 'YES' : 'NO';
+  print(outputMsg);
+  output.add(outputMsg);
+
+  // Display the peg moves of the solution.
+  final int pegMovesSolutionSize = pegMovesSolution[0][0];
+  for (int i = pegMovesSolutionSize; i > 0; i--) {
+    final List<int> tempMove = pegMovesSolution[i];
+    final int r1 = tempMove[0];
+    final int c1 = tempMove[1];
+    final int r2 = tempMove[2];
+    final int c2 = tempMove[3];
+
+    final String outputMove = '${r1 + 1},${c1 + 1}->${r2 + 1},${c2 + 1}';
+    print(outputMove);
+  }
+  print('\n');
+
+  print(output);
+  print('\n');
 }
 
-bool solve({
+bool solveSolitairePeg({
   required List<List<String>> boardState,
-  required List<List<int>> pegCoordinates,
-  required int noOfPegs,
+  required List<int> metadata,
 }) {
+  print('boardState: $boardState');
+  print('finalHole: $finalHoleRow, $finalHoleColumn');
+  print('metadata: $metadata');
+  print('\n');
+
   // Base case/s.
   // Case 1: There are no pegs to be moved.
+  final int noOfPegs = metadata[0];
   if (noOfPegs == 0) return false;
 
   // Case 2: One peg is remaining. Check if its coordinate is equal
   // to the finalHole coordinate.
-  if (noOfPegs == 1) {
-    final int row = pegCoordinates[0][0];
-    final int column = pegCoordinates[0][1];
+  final int tempFinalHoleRow = metadata[1];
+  final int tempFinalHoleColumn = metadata[2];
 
-    if ((row == finalHole[0]) && (column == finalHole[1])) return true;
+  if ((noOfPegs == 1) &&
+      (tempFinalHoleRow == finalHoleRow) &&
+      (tempFinalHoleColumn == finalHoleColumn)) {
+    return true;
   }
 
-  for (int i = 0; i < noOfPegs; i++) {
-    // Check if there is an adjacent peg relative to the current peg.
-    // The order of peg-checking: North, East, South, and then West.
-    // While doing this, check also if the hole destination is available.
+  for (int r = 0; r < noOfRows; r++) {
+    for (int c = 0; c < nowOfColumns; c++) {
+      // Check if it is a peg.
+      if (boardState[r][c] == pegHole) {
+        // Check if there is an adjacent peg relative to the current peg.
+        // While doing this, check also if the hole destination is available.
+        // The order of peg-checking: North, East, South, and then West.
 
-    // The r and c coordinate of the current peg.
-    final int currentPegRow = pegCoordinates[i][0];
-    final int currentPegColumn = pegCoordinates[i][1];
+        // Check North.
+        if ((r >= 2) &&
+            (boardState[r - 1][c] == pegHole) &&
+            (boardState[r - 2][c] == emptyHole)) {
+          // Perform deep copy on the list parameters.
+          List<List<String>> newBoardState = deepCopy2DList<String>(boardState);
+          List<int> newMetaData = [...metadata];
 
-    // Check North.
-    if ((currentPegRow > 1) &&
-        (boardState[currentPegRow - 1][currentPegColumn] == pegHole) &&
-        (boardState[currentPegRow - 2][currentPegColumn] == emptyHole)) {
-      // Perform deep copy on the list parameters.
-      List<List<String>> newBoardState = deepCopy2DList<String>(boardState);
-      List<List<int>> newPegCoordinates = deepCopy2DList<int>(pegCoordinates);
+          // Update the boardState.
+          // Make the coordinate of the jumping peg empty.
+          newBoardState[r][c] = emptyHole;
+          // Delete the jumped-over hole.
+          newBoardState[r - 1][c] = emptyHole;
+          // Put the peg on the new coordinate.
+          newBoardState[r - 2][c] = pegHole;
 
-      // Update the boardState.
-      // Make the coordinate of the jumping peg empty.
-      newBoardState[currentPegRow][currentPegColumn] = emptyHole;
-      // Delete the jumped-over hole.
-      newBoardState[currentPegRow - 1][currentPegColumn] = emptyHole;
-      // Put the peg on the new coordinate.
-      newBoardState[currentPegRow - 2][currentPegColumn] = pegHole;
+          newMetaData[0] = newMetaData[0] - 1; // Update the noOfPegs.
+          // Determine the tempFinalHole coordinate.
+          newMetaData[1] = r - 2;
+          newMetaData[2] = c;
 
-      // Update the pegCoordinates.
-      // Set the coordinate of the jumping peg to its destination peg coordinate.
-      newPegCoordinates[i] = [currentPegRow - 2, currentPegColumn];
-      // Delete the coordinate of the jumped-over peg.
+          bool isSolvable = solveSolitairePeg(
+            boardState: newBoardState,
+            metadata: newMetaData,
+          );
 
-      bool returnValue = solve(
-        boardState: newBoardState,
-        pegCoordinates: newPegCoordinates,
-        noOfPegs: noOfPegs - 1,
-      );
+          if (isSolvable) {
+            pegMovesSolution.add([r, c, r - 2, c]); // Add the move details.
+            // Update the size.
+            pegMovesSolution[0][0] = pegMovesSolution[0][0] + 1;
+
+            return true;
+          }
+        }
+
+        // Check East.
+        if ((c <= 4) &&
+            (boardState[r][c + 1] == pegHole) &&
+            (boardState[r][c + 2] == emptyHole)) {
+          // Perform deep copy on the list parameters.
+          List<List<String>> newBoardState = deepCopy2DList<String>(boardState);
+          List<int> newMetaData = [...metadata];
+
+          // Update the boardState.
+          // Make the coordinate of the jumping peg empty.
+          newBoardState[r][c] = emptyHole;
+          // Delete the jumped-over hole.
+          newBoardState[r][c + 1] = emptyHole;
+          // Put the peg on the new coordinate.
+          newBoardState[r][c + 2] = pegHole;
+
+          newMetaData[0] = newMetaData[0] - 1; // Update the noOfPegs.
+          // Determine the tempFinalHole coordinate.
+          newMetaData[1] = r;
+          newMetaData[2] = c + 2;
+
+          bool isSolvable = solveSolitairePeg(
+            boardState: newBoardState,
+            metadata: newMetaData,
+          );
+
+          if (isSolvable) {
+            pegMovesSolution.add([r, c, r, c + 2]); // Add the move details.
+            // Update the size.
+            pegMovesSolution[0][0] = pegMovesSolution[0][0] + 1;
+
+            return true;
+          }
+        }
+
+        // Check South.
+        if ((r <= 4) &&
+            (boardState[r + 1][c] == pegHole) &&
+            (boardState[r + 2][c] == emptyHole)) {
+          // Perform deep copy on the list parameters.
+          List<List<String>> newBoardState = deepCopy2DList<String>(boardState);
+          List<int> newMetaData = [...metadata];
+
+          // Update the boardState.
+          // Make the coordinate of the jumping peg empty.
+          newBoardState[r][c] = emptyHole;
+          // Delete the jumped-over hole.
+          newBoardState[r + 1][c] = emptyHole;
+          // Put the peg on the new coordinate.
+          newBoardState[r + 2][c] = pegHole;
+
+          newMetaData[0] = newMetaData[0] - 1; // Update the noOfPegs.
+          // Determine the tempFinalHole coordinate.
+          newMetaData[1] = r + 2;
+          newMetaData[2] = c;
+
+          bool isSolvable = solveSolitairePeg(
+            boardState: newBoardState,
+            metadata: newMetaData,
+          );
+
+          if (isSolvable) {
+            pegMovesSolution.add([r, c, r + 2, c]); // Add the move details.
+            // Update the size.
+            pegMovesSolution[0][0] = pegMovesSolution[0][0] + 1;
+
+            return true;
+          }
+        }
+
+        // Check West.
+        if ((c >= 2) &&
+            (boardState[r][c - 1] == pegHole) &&
+            (boardState[r][c - 2] == emptyHole)) {
+          // Perform deep copy on the list parameters.
+          List<List<String>> newBoardState = deepCopy2DList<String>(boardState);
+          List<int> newMetaData = [...metadata];
+
+          // Update the boardState.
+          // Make the coordinate of the jumping peg empty.
+          newBoardState[r][c] = emptyHole;
+          // Delete the jumped-over hole.
+          newBoardState[r][c - 1] = emptyHole;
+          // Put the peg on the new coordinate.
+          newBoardState[r][c - 2] = pegHole;
+
+          newMetaData[0] = newMetaData[0] - 1; // Update the noOfPegs.
+          // Determine the tempFinalHole coordinate.
+          newMetaData[1] = r;
+          newMetaData[2] = c - 2;
+
+          bool isSolvable = solveSolitairePeg(
+            boardState: newBoardState,
+            metadata: newMetaData,
+          );
+
+          if (isSolvable) {
+            pegMovesSolution.add([r, c, r, c - 2]); // Add the move details.
+            // Update the size.
+            pegMovesSolution[0][0] = pegMovesSolution[0][0] + 1;
+
+            return true;
+          }
+        }
+      }
     }
-
-    // Check East.
-
-    // Check South.
-
-    // Check West.
   }
 
-  // The peg solitaire is unsolvable.
+  // No solution was found for the current boardState.
   return false;
-}
-
-int deleteElem({
-  required List<Object> list,
-  required int index,
-  required int noOfPegs,
-}) {
-  for (int i = index; i < (noOfPegs - 1); i++) {
-    list[i] = list[i + 1];
-    list[i + 1] = [-1, -1]; // Optional to code in MIPS.
-  }
-
-  noOfPegs--;
-  return noOfPegs;
 }
 
 // This explicit implementation will not be coded in MIPS.
