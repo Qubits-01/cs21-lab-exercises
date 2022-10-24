45c8e9e68bf6381566bc2cb3bb8f6bba85cb35f9
Marc Peejay Viernes <mpvviernes@gmail.com>
2022-10-24 08:58:03 +0800

    Initialize peg solitaire setup

---
+++ machine_prob_1/peg_solitaire/bin/peg_solitaire.dart
@@ -0,0 +1,5 @@
+import 'package:peg_solitaire/peg_solitaire.dart' as peg_solitaire;
+
+void main(List<String> arguments) {
+  print('Hello world: ${peg_solitaire.calculate()}!');
+}
---
+++ machine_prob_1/peg_solitaire/test/peg_solitaire_test.dart
@@ -0,0 +1,8 @@
+import 'package:peg_solitaire/peg_solitaire.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
