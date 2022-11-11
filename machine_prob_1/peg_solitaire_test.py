ceacc62fa252170768e958302cf364806d24f223
Marc Peejay Viernes <mpvviernes@gmail.com>
2022-11-11 11:17:19 +0800

    Add automated tester

--- machine_prob_1/peg_solitaire_test.py
+++ machine_prob_1/peg_solitaire_test.py
@@ -1,79 +1,97 @@
-import asyncio
-import os
 import subprocess
 import time
+import argparse
 from colorama import Fore
 
+test_starting_time = time.time()
 
-async def main():
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', '--verbose', help='Shows a detailed view of the test case running.')
+args = parser.parse_args()
+
+
+def main():
+    if args.verbose is not None:
+        is_verbose = args.verbose.strip().lower() in ['yes', 'y', '1', 'true', 'oo', 'yup', 'wen', 'sige']
+    else:
+        is_verbose = False
+
     print(Fore.BLUE + 'READING TEST CASES MASTER LIST FILE...\n')
-
     test_cases_master_list_file = open('test_cases_master_list.txt', 'r')
     test_cases_master_list_lines = test_cases_master_list_file.readlines()
     test_cases_master_list_file.close()
 
-    # print(test_cases_master_list_lines)
-    # print(len(test_cases_master_list_lines))
+    no_of_test_cases = (len(test_cases_master_list_lines) + 1) // 10
+    print(f'RUNNING {no_of_test_cases} number of test case/s.')
 
-    temp_input_file = open('temp_input.txt', 'w')
-    # temp_output_data_file = open('temp_output_data.txt', 'w+')
     test_case_num = 1
+    n_successful_test_cases = 0
 
     for i in range(0, len(test_cases_master_list_lines), 10):
+        if is_verbose:
+            print(Fore.YELLOW + '==================================================')
+
         print(Fore.GREEN + f'Test Case {test_case_num}: ', end='')
 
         # Read the board state.
-        temp_input = []
+        temp_input = ''
         for j in range(1, 8):
-            temp_input.append(test_cases_master_list_lines[i + j])
-            temp_input_file.writelines(temp_input)
+            temp_input += test_cases_master_list_lines[i + j]
 
         # Read the expected output (YES or NO).
         expected_output = test_cases_master_list_lines[i + 8].strip()
-        # print('expected output:', expected_output)
 
-        # print(f'{test_case_num}. {temp_input}')
-        # print(temp_output)
+        # Optional: Display the input.
+        if is_verbose:
+            test_case_description = test_cases_master_list_lines[i]
+            print(test_case_description[test_case_description.index(' '):].strip())
+            print(Fore.CYAN + temp_input.strip())
+            print(Fore.LIGHTCYAN_EX + 'Expected output: ', end='')
+            print(Fore.CYAN + f'{expected_output}\n')
 
-        # Run MIPS code through Mars' CLI.
-        command = 'java -jar Mars4_5.jar p sm nc cs21project1C.asm < input.txt'
+        # Run MIPS code through Mars' CLI
+        command = 'java -jar Mars4_5.jar p sm nc cs21project1C.asm'
         starting_time = time.time()
-        p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
-        (output, err) = p.communicate()
-        p_status = p.wait()
-        print('outptut', output)
+        p = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        (output, err) = p.communicate(temp_input.encode('utf-8').strip())
+        p_status = p.wait()  # Wait for the subprocess to finish.
         execution_time = time.time() - starting_time
 
-        # Read the first line of temp_output_data.txt.
-        temp_output_data_file = open('temp_output_data.txt', 'r')
-        actual_output = temp_output_data_file.readline().strip()
-        temp_output_data_file.close()
-        # print('actual output:', actual_output)
+        actual_output = output.decode('utf-8')
+        yes_or_no = actual_output.strip().split()[0].strip()
+
+        # Optional: Display your output.
+        if is_verbose:
+            print(Fore.LIGHTCYAN_EX + 'Your output:')
+            print(Fore.CYAN + actual_output.strip(), end='\n')
+
+        spacing = ''
+        if not is_verbose:
+            spacing = ' '
+        else:
+            print()
 
         if execution_time > 10:
-            print(Fore.RED + f'FAILED | ? {actual_output} | TLE ({execution_time} s)')
-        elif actual_output != expected_output:
-            print(Fore.RED + f'FAILED | WA ({actual_output}) | {execution_time} s')
+            is_wrong_answer = True if yes_or_no != expected_output else False
+            print(Fore.RED + f'{spacing}FAILED  |  {"WA (" if is_wrong_answer else ""}{yes_or_no}{")" if is_wrong_answer else " "}  |  TLE ({execution_time} s)')
+        elif yes_or_no != expected_output:
+            print(Fore.RED + f'{spacing}FAILED  |  WA ({yes_or_no})  |  {execution_time} s')
         else:
-            print(Fore.LIGHTGREEN_EX + f'PASSED | {actual_output} | {execution_time} s')
+            if yes_or_no == 'NO':
+                yes_or_no = 'NO '
 
-        print('expected output:', expected_output)
-        print('actual output:', actual_output)
+            print(Fore.LIGHTGREEN_EX + f'{spacing}PASSED  |  {yes_or_no}  |  {execution_time} s')
+            n_successful_test_cases += 1
 
-
-
-
-
-
+        if is_verbose:
+            print(Fore.YELLOW + '==================================================\n')
 
         test_case_num += 1
 
-    print(Fore.BLUE + '\nCLEANING UP...\n')
-    temp_input_file.close()
-    os.remove('temp_input.txt')
-    # temp_output_data_file.close()
-    # os.remove('temp_output_data.txt')
+    print(Fore.BLUE + f'Time elapsed: {time.time() - test_starting_time} s')
+    print(f'{n_successful_test_cases} out of {no_of_test_cases} test case/s passed ( {round((n_successful_test_cases / no_of_test_cases) * 100, 2)}% ).')
+    print('\nDONE\n')
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
